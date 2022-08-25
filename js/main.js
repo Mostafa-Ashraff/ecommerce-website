a8c101f671dc93818093745eb6192e516cb257fc
Mostafa-Ashraff <94311378+Mostafa-Ashraff@users.noreply.github.com>
2022-08-25 23:54:53 +0200

    products slider

--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
 //navigation burger menu toggle
 const menu = document.querySelector('.nav-burger');
+const nav = document.querySelector('.navbar');
 let menuOpen= false;
 menu.addEventListener('click', ()=>{
     if(menuOpen === false){
         menu.classList.add('active');
+        nav.classList.add('active');
         menuOpen = true;
     }
     else{
         menu.classList.remove('active');
+        nav.classList.remove('active');
         menuOpen = false;
     }
 })
+
+//glider setup
+
+
+
+function btns(){
+    let prevBtn = document.querySelector('.slick-prev');
+    prevBtn.innerHTML = `<span><i class="fa-solid fa-angle-left"></i></span>`;
+    
+    let prevNext = document.querySelector('.slick-next');
+    prevNext.innerHTML = `<span><i class="fa-solid fa-angle-right"></i></span>`;
+    
+};
+setInterval(btns, 1)
