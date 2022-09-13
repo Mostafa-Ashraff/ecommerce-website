97ee3bd85ffe77dfe1f1e1d9a0140c051c915659
Mostafa-Ashraff <94311378+Mostafa-Ashraff@users.noreply.github.com>
2022-09-13 22:58:57 +0200

    create cart item js

--- js/main.js
+++ js/main.js
@@ -19,12 +19,70 @@ menu.addEventListener('click', ()=>{
 
 
 
-function btns(){
-    let prevBtn = document.querySelector('.slick-prev');
-    prevBtn.innerHTML = `<span><i class="fa-solid fa-angle-left"></i></span>`;
+
+
+cards = document.querySelectorAll('.slider-card');
+
+cards.forEach(card => {
+    card.addEventListener('click', createCartItem)
+});
+
+function createCartItem(){
     
-    let prevNext = document.querySelector('.slick-next');
-    prevNext.innerHTML = `<span><i class="fa-solid fa-angle-right"></i></span>`;
-    
-};
-setInterval(btns, 1)
+        const cartItem = document.createElement('div');
+        cartItem.className = 'cart-item';
+
+        const imgDiv = document.createElement('div');
+
+        let itemImg = document.createElement('img');
+        itemImg.src =  card.firstElementChild.src
+        imgDiv.appendChild(itemImg);
+
+        cartItem.appendChild(imgDiv);
+
+        const detailsDiv = document.createElement('div');
+        detailsDiv.className = 'product-details';
+        const itemHd = document.createElement('h3');
+        itemHd.className = 'product-name';
+        itemHd.textContent = card.children[1].children[0].textContent;
+        const itemPrice = document.createElement('p');
+        itemPrice.className = 'product-price';
+        itemPrice.textContent = card.children[1].children[1].textContent;
+
+
+        detailsDiv.appendChild(itemHd);
+        detailsDiv.appendChild(itemPrice);
+
+        cartItem.appendChild(detailsDiv);
+
+        const inputQuan = document.createElement('input');
+        inputQuan.className = 'product-quantity';
+        cartItem.appendChild(inputQuan);
+
+        const sbTotal = document.createElement('p');
+        sbTotal.className = 'product-subtotal';
+        cartItem.appendChild(sbTotal);
+
+        const removeSpan = document.createElement('span');
+        removeSpan.innerHTML = '<i class="fa-solid fa-x remove-item"></i>';
+        cartItem.appendChild(removeSpan);
+
+
+        const hr = document.createElement('hr');
+        cartItem.appendChild(hr);
+
+        const cartDiv = document.querySelector('.cart');
+
+        cartDiv.appendChild(cartItem);
+}
+/*
+const totalDiv = document.createElement('div');
+totalDiv.className = 'total-price';
+const total = document.createElement('h2');
+const totalPrice = document.createElement('p');
+totalPrice.className = 'product-price';
+
+const paymentDiv = document.createElement('div');
+const total = document.createElement('h2');
+const totalPrice = document.createElement('p');
+totalPrice.className = 'product-price';*/
