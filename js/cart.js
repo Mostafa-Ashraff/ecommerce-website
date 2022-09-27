1c319c6918b509500c262a2948ee06bdaed2b072
Mostafa-Ashraff <94311378+Mostafa-Ashraff@users.noreply.github.com>
2022-09-28 00:40:32 +0200

    decrement function

--- js/cart.js
+++ js/cart.js
@@ -7,34 +7,63 @@ let removeItem = ()=>{
     }))
 } 
 removeItem();
-basket.forEach((item)=>{
-    console.log(item);
-    let cartItem = document.createElement('div');
-    cartItem.className = 'cart-item';
-    cartItem.setAttribute('id', `${item.id}`)
-    cartItem.innerHTML = `
-        <div>
-        <img src="${item.img}" alt="">    
-        </div>
-        <div class="product-details">
-            <h3 class="product-name">${item.name}</h3>
-            <p class="product-price">$${item.price}</p>
-        </div>
-        <div class="cart-btns">
-            <i  class="bi bi-dash-lg"></i>
-            <div id="" class="quantity">${item.quantity}</div>
-            <i onclick="increment(${item.id})" class="bi bi-plus-lg"></i>
-        </div>
-        <p class="product-subtotal">$ ${item.price * item.quantity}</p>
-        <span><i class="fa-solid fa-x remove-item"></i></span>
-        </div>
-    `
-    console.log(cartItem);
 
-    cartItemsDiv.appendChild(cartItem);
+let decrement = (id)=>{
+    
+    let selectedItem = id;
+    let ItemInBasket = basket.find((x) => x.id === selectedItem.id);
+    ItemInBasket.quantity -= 1;
+    if(ItemInBasket.quantity ===0){
+        removeItem();
+        //document.getElementById(`${ItemInBasket.id}`).remove();
+        console.log(selectedItem.id);
+        console.log(ItemInBasket);
+    }
+    
+    
+    
+    
+    
+    quantity = ItemInBasket.quantity;
+    localStorage.setItem("data", JSON.stringify(basket));
+    cartItemsDiv.innerHTML = '';
+    generateCartItems();
+};
 
-    removeItem();
-})/*
+
+function generateCartItems(){
+    basket.forEach((item)=>{
+        //console.log(item);
+        let cartItem = document.createElement('div');
+        cartItem.className = 'cart-item';
+        cartItem.setAttribute('id', `${item.id}`)
+        cartItem.innerHTML = `
+            <div>
+            <img src="${item.img}" alt="">    
+            </div>
+            <div class="product-details">
+                <h3 class="product-name">${item.name}</h3>
+                <p class="product-price">$${item.price}</p>
+            </div>
+            <div class="cart-btns">
+                <i onclick="decrement(${item.id})" class="bi bi-dash-lg"></i>
+                <div id="quantity" class="quantity">${item.quantity}</div>
+                <i onclick="increment(${item.id})" class="bi bi-plus-lg"></i>
+            </div>
+            <p class="product-subtotal">$ ${(item.price * item.quantity).toFixed(2)}</p>
+            <span><i class="fa-solid fa-x remove-item"></i></span>
+            </div>
+        `
+        //console.log(cartItem);
+    
+        cartItemsDiv.appendChild(cartItem);
+    
+        removeItem();
+    })
+};
+generateCartItems();
+
+/*
 let generateCartItem = ()=>{
     let cartItem = document.createElement('div');
     cartItem.className = 'cart-item';
@@ -67,24 +96,27 @@ generateCartItem();
 
 let increment = (id)=>{
     let selectedItem = id;
-    let search = basket.find((x) => x.id === selectedItem.id);
-    console.log(search);
-    if (search === undefined) {
-      basket.push({
-        id: selectedItem.id,
-        quantity: 1,
-      });
-    } else {
-    search.quantity += 1;
-}};
-
-let addItemsBtn = document.querySelectorAll('.bi-plus-lg');
-addItemsBtn.forEach(btn => btn.addEventListener('click', increment()))
-
-let decrement= ()=>{
-    
+    console.log(selectedItem);
+    let ItemInBasket = basket.find((x) => x.id === selectedItem.id);
+    ItemInBasket.quantity += 1;
+    console.log(ItemInBasket);
+    quantity = ItemInBasket.quantity;
+    localStorage.setItem("data", JSON.stringify(basket));
+    cartItemsDiv.innerHTML = '';
+    generateCartItems();
 };
 
+let addItemsBtns = document.querySelectorAll('.bi-plus-lg');
+addItemsBtns.forEach(btn => btn.addEventListener('click', increment()));
+
+
+let decreaseItemsBtns = document.querySelectorAll('.bi-dash-lg');
+console.log(decreaseItemsBtns);
+//decreaseItemsBtn.forEach(btn => btn);
+
+
 let update= ()=>{
-    
+    let selectedItem = id;
+
+
 };
